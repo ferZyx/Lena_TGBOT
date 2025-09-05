8be58d970fd169a9a3858d6e40dca1f48779ed6b
Vladislav Sorokin <sorokin.wlad@yandex.ru>
2025-09-05 20:16:42 +0500

    main | add ecosystem configuration for tolyan-lena-bot

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: "tolyan-lena-bot",
+            script: "./app.js",
+            log_date_format: "YYYY-MM-DD HH:mm:ss",
+            autorestart: true,
+        }
+    ]
+}
