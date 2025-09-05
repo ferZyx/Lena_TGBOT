873d3ba2861446251ac8fe0167598d859232e7d5
Vladislav Sorokin <sorokin.wlad@yandex.ru>
2025-09-05 20:19:31 +0500

    main | update ecosystem configuration to use npm start script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,8 @@ module.exports = {
     apps: [
         {
             name: "tolyan-lena-bot",
-            script: "./app.js",
+            script: "npm",
+            args: "run start",
             log_date_format: "YYYY-MM-DD HH:mm:ss",
             autorestart: true,
         }
